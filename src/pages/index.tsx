48d0d94f00127e5419f0b446e08f0a0e91754f18
nestonzin <dinestoor@outlook.com>
2022-10-19 15:55:43 -0300

    feat: :sparkles: Feature/Layout

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return <h1>ola mundo!</h1>
+}
