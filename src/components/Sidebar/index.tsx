e8f27259623ec6684c37a982c55996bc93a25b42
Buletiss <vinicius1997feitosa@gmail.com>
2022-10-26 13:11:36 -0300

    fix: :bug: add filtro de jogos completos

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
+import { Avatar, Box, Flex, Text, Spinner } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import api from '../../services/api';
 
@@ -12,6 +12,7 @@ interface Match {
 
 interface Schedule {
   startTime: Date;
+  state: string;
   league: {
     name: string;
   };
@@ -42,35 +43,37 @@ export const Sidebar = () => {
       justifyContent="space-between"
       overflowY="scroll"
     >
-      {schedules.map((schedule, index) => (
-        <Flex
-          key={`${schedule}-${index}`}
-          flexDirection="column"
-          alignItems="center"
-          bg="#D9D9D9"
-          p="1rem"
-          cursor="pointer"
-          border="1px"
-        >
-          <Text fontWeight="500">{new Date(schedule.startTime).toLocaleString('pt-BR', { weekday: 'long' })}</Text>
-          <Text fontWeight="500">{schedule.league.name}</Text>
-          <Flex justifyContent="space-between" w="100%" alignItems="center">
-            <Box>
-              <Avatar size="sm" src={schedule.match.teams[0].image} />
-              <Text fontWeight="bold" lineHeight="30px" fontSize="20px">
-                {schedule.match.teams[0].code}
-              </Text>
-            </Box>
-            <Text fontWeight="500">vs</Text>
-            <Box>
-              <Avatar size="sm" src={schedule.match.teams[1].image} />
-              <Text fontWeight="bold" lineHeight="30px" fontSize="20px">
-                {schedule.match.teams[1].code}
-              </Text>
-            </Box>
+      {schedules
+        .filter(schedule => schedule.state.includes('unstarted'))
+        .map((schedule, index) => (
+          <Flex
+            key={`${schedule}-${index}`}
+            flexDirection="column"
+            alignItems="center"
+            bg="#D9D9D9"
+            p="1rem"
+            cursor="pointer"
+            border="1px"
+          >
+            <Text fontWeight="500">{new Date(schedule.startTime).toLocaleString('pt-BR', { calendar: 'long' })}</Text>
+            <Text fontWeight="500">{schedule.league.name}</Text>
+            <Flex justifyContent="space-between" w="100%" alignItems="center">
+              <Box>
+                <Avatar size="sm" src={schedule.match.teams[0].image} />
+                <Text fontWeight="bold" lineHeight="30px" fontSize="20px">
+                  {schedule.match.teams[0].code}
+                </Text>
+              </Box>
+              <Text fontWeight="500">vs</Text>
+              <Box>
+                <Avatar size="sm" src={schedule.match.teams[1].image} />
+                <Text fontWeight="bold" lineHeight="30px" fontSize="20px">
+                  {schedule.match.teams[1].code}
+                </Text>
+              </Box>
+            </Flex>
           </Flex>
-        </Flex>
-      ))}
+        ))}
     </Flex>
   );
 };
